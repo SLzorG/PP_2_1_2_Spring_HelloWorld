f3673d41ea954e91cac80a38c434c76a71853c3e
SlZorg <zoorgbog@mail.ru>
2023-04-13 18:46:41 +0300

    2.1.3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat bean3 = (Cat) applicationContext.getBean("сat");
+        Cat bean4 = (Cat) applicationContext.getBean("сat");
+
+        System.out.println("Для пары HelloWorld - " + (bean1 == bean2));
+        System.out.println("Для пары cat - " + (bean3 == bean4));
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+    private String message;
+
+    public void setMessage(String say) {
+        this.message = say;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
